f2fc81b6360f732542d591182fc1c442d6f23d66
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-01-23 14:33:10 -0300

    Atualizando

--- estudandopython/Exercicios_curso_em_video/82.py
+++ estudandopython/Exercicios_curso_em_video/82.py
@@ -1,3 +1,16 @@
 """
 82. Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 """
+lista = []
+listaPar = []
+listaImpar = []
+continua = ' '
+while True:
+    n = int(input('Digite um número:'))
+    lista.append(n)
+    if n % 2 == 0:
+        listaPar.append(n)
+    else:
+        listaImpar.append(n)
+    while continua not in 'SN':
+        continua = str(input('Quer continuar? [S/N]')).upper().strip()[0]
