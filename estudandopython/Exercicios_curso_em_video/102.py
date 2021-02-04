2117cd19aa371e74453bb952d099830ac475e019
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-02-03 22:25:16 -0300

    Exercicio 102

---
+++ estudandopython/Exercicios_curso_em_video/102.py
@@ -0,0 +1,23 @@
+"""
+102. Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+"""
+def fatorial(num = 1, show = False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param num: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+        if show :
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end ='')
+    return f
+#  Programa Principal
+print('-' * 40)
+print(fatorial(5,True))
