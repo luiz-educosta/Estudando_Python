686f1c9b24d10521b4b4f122f24a9e9f338270b1
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-02-07 18:29:38 -0300

    Alterando o nome das pastas

--- estudandopython/Exercicios_curso_em_video/107-108-109-110/109.py
+++
@@ -1,9 +0,0 @@
-"""
-109.  Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
-"""
-import moeda
-preco = float(input('Digite o preço: R$'))
-print(f'A medade de {moeda.moeda(preco)} é {moeda.metade(preco, True)}')
-print(f'O dobro de {moeda.moeda(preco)} é {moeda.dobro(preco, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(preco,10, True)}')
-print(f'Diminuindo 10%, temos {moeda.diminuir(preco,10, True)}')
--- estudandopython/Exercicios_curso_em_video/107-108-109-110/110.py
+++
@@ -1,7 +0,0 @@
-"""
-110. Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
-"""
-import moeda
-
-preco = float(input('Digite o preço: R$'))
-moeda.resumo(preco, 80, 35)
