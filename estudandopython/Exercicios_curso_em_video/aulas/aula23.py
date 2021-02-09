c7e2a59ca9d9d571c3dfdbfc8224781f27dc11ff
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-02-09 19:07:19 -0300

    Aula 23

---
+++ estudandopython/Exercicios_curso_em_video/aulas/aula23.py
@@ -0,0 +1,37 @@
+"""
+
+Tratamento de Erros e Exceções
+ - Erro de sintaxe: primt(x)
+ - Erro de exceção: NameError, ValueError, ZeroDivisionError, IndexError, KeyError, ModuleNotFoundError...
+usar comando:
+    try:
+        operação
+    except TypeError:
+        falhou
+    except ValueError:
+        falhou
+    except OSError:
+        falhou
+    else:
+        deu certo
+    finally:
+        independente se deu certo/falha
+"""
+#  print(x) sem definir a váriavel
+
+try:
+    a = int(input('Númerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou ')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito obrigado!')
