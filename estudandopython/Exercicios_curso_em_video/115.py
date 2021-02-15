754d51f18926168b6b662b69de344ac80dbd34eb
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-02-14 21:14:33 -0300

    Adicionando 115.py

---
+++ estudandopython/Exercicios_curso_em_video/115.py
@@ -0,0 +1,4 @@
+"""
+115. Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
+O sistema só vai ter 2 opções: Cadastrar uma nova pessoa e listar todas as pessoas cadastradas.
+"""
