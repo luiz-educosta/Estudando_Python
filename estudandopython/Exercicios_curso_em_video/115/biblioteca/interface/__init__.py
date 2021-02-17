c4c12da8790dd13649fa3ed6104a87f6162c0b48
luiz-educosta <luizeduardorodrigues@tai.inatel>
2021-02-16 21:20:32 -0300

    Exercicio 115a

--- estudandopython/Exercicios_curso_em_video/115/biblioteca/interface/__init__.py
+++ estudandopython/Exercicios_curso_em_video/115/biblioteca/interface/__init__.py
@@ -1,8 +1,33 @@
+def leiaInt(msg):
+    while True:
+        try:
+            num = int(input(msg))
+        except(ValueError, TypeError):
+            print('\033[31mErro: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except(KeyboardInterrupt):
+            print('\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return num
+
+            
 def linha (tam = 42):
     return '-' * tam
 
 
 def cabecalho (txt):
     print(linha())
-    print(txt)
+    print(txt.center(42))
     print(linha())
+
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
+    opc = leiaInt('\033[32mSua opção:\033[m')
+    return opc
--- estudandopython/Exercicios_curso_em_video/115/biblioteca/sistema.py
+++ estudandopython/Exercicios_curso_em_video/115/biblioteca/sistema.py
@@ -1,3 +1,17 @@
 from interface import *
+from time import sleep
 
-cabecalho('teste')
+
+while True:
+        
+    resposta = menu(['Ver pessoas cadastradas','Cadastrar nova pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('opção 1')
+    elif resposta == 2:
+        cabecalho('Opção 2')
+    elif resposta == 3:
+        cabecalho('Saindo do Sistema... Até logo')
+        break
+    else:
+        print('\033[31mErro digite uma opção válida!!!\033[m')
+    sleep(2)
