b2a0abd7bef01798ddc091902ea0895476dc6fde
Luiz Eduardo <luizeduardorodrigues@tai.inatel.br>
2021-03-23 17:55:15 -0300

    Melhorando o exercicio 2

--- estudandopython/GeekUniversity/POO_python_br/ex02.py
+++ estudandopython/GeekUniversity/POO_python_br/ex02.py
@@ -5,17 +5,26 @@ b.Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 """
 class Quadrado:
     def __init__(self, lado):
-        self.lado = lado
+        self.__lado = lado
 
-    
-    def mudaLado(self, valor):
-        self.lado = valor
 
-    
-    def retornaLado(self):
-        return self.lado
+    @property
+    def lado(self):
+        return print(f'O quadrado encontra-se com a medida de {self.__lado}')
+
+
+    @lado.setter
+    def lado(self, valor):
+        self.__lado = valor
+        print(f'Valor do lado mudou para {self.__lado}')
 
 
     def calculaArea(self):
-        return self.lado * self.lado
-        
+        area =  self.__lado * self.__lado
+        return print(f'Calculando a área do quadrado {self.__lado} x {self.__lado} = {area}')
+         
+
+q = Quadrado(5)
+q.calculaArea()
+q.lado
+q.lado = 10
